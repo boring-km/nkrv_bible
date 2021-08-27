77fb13482727c76256c236a36f3d2f0b7c65ff78
boring-km <kms0644804@naver.com>
2021-08-27 23:53:18 +0900

    [NB-01] 구약성서 신약성서 enum 한글 매핑

--- lib/data/new_testament.dart
+++ lib/data/new_testament.dart
@@ -27,3 +27,67 @@ enum NewTestament {
   Jud,
   Rev
 }
+
+extension NewTestamentExtension on NewTestament {
+  String get label {
+    switch (this) {
+      case NewTestament.Mat:
+        return '마태복음';
+      case NewTestament.Mar:
+        return '마가복음';
+      case NewTestament.Luk:
+        return '누가복음';
+      case NewTestament.Joh:
+        return '요한복음';
+      case NewTestament.Act:
+        return '사도행전';
+      case NewTestament.Rom:
+        return '로마서';
+      case NewTestament.Co1:
+        return '고린도전서';
+      case NewTestament.Co2:
+        return '고린도후서';
+      case NewTestament.Gal:
+        return '갈라디아서';
+      case NewTestament.Eph:
+        return '에베소서';
+      case NewTestament.Phi:
+        return '빌립보서';
+      case NewTestament.Col:
+        return '골로새서';
+      case NewTestament.Th1:
+        return '데살로니가전서';
+      case NewTestament.Th2:
+        return '데살로니가후서';
+      case NewTestament.Ti1:
+        return '디모데전서';
+      case NewTestament.Ti2:
+        return '디모데후서';
+      case NewTestament.Tit:
+        return '디도서';
+      case NewTestament.Phm:
+        return '빌레몬서';
+      case NewTestament.Heb:
+        return '히브리서';
+      case NewTestament.Jam:
+        return '야고보서';
+      case NewTestament.Pe1:
+        return '베드로전서';
+      case NewTestament.Pe2:
+        return '베드로후서';
+      case NewTestament.Jo1:
+        return '요한1서';
+      case NewTestament.Jo2:
+        return '요한2서';
+      case NewTestament.Jo3:
+        return '요한3서';
+      case NewTestament.Jud:
+        return '유다서';
+      case NewTestament.Rev:
+        return '요한계시록';
+
+      default:
+        return '신약성서';
+    }
+  }
+}
--- lib/data/old_testament.dart
+++ lib/data/old_testament.dart
@@ -39,3 +39,90 @@ enum OldTestament {
   Zec,
   Mal
 }
+
+extension OldTestamentExtension on OldTestament {
+  String get label {
+    switch (this) {
+      case OldTestament.Gen:
+        return '창세기';
+      case OldTestament.Exo:
+        return '출애굽기';
+      case OldTestament.Lev:
+        return '레위기';
+      case OldTestament.Num:
+        return '민수기';
+      case OldTestament.Deu:
+        return '신명기';
+      case OldTestament.Jos:
+        return '여호수아';
+      case OldTestament.Jdg:
+        return '사사기';
+      case OldTestament.Rut:
+        return '룻기';
+      case OldTestament.Sa1:
+        return '사무엘상';
+      case OldTestament.Sa2:
+        return '사무엘하';
+      case OldTestament.Ki1:
+        return '열왕기상';
+      case OldTestament.Ki2:
+        return '열왕기하';
+      case OldTestament.Ch1:
+        return '역대상';
+      case OldTestament.Ch2:
+        return '역대하';
+      case OldTestament.Ezr:
+        return '에스라';
+      case OldTestament.Neh:
+        return '느헤미야';
+      case OldTestament.Est:
+        return '에스더';
+      case OldTestament.Job:
+        return '욥기';
+      case OldTestament.Psa:
+        return '시편';
+      case OldTestament.Pro:
+        return '잠언';
+      case OldTestament.Ecc:
+        return '전도서';
+      case OldTestament.Sol:
+        return '아가';
+      case OldTestament.Isa:
+        return '이사야';
+      case OldTestament.Jer:
+        return '예레미야';
+      case OldTestament.Lam:
+        return '예레미야애가';
+      case OldTestament.Eze:
+        return '에스겔';
+      case OldTestament.Dan:
+        return '다니엘';
+      case OldTestament.Hos:
+        return '호세아';
+      case OldTestament.Joe:
+        return '요엘';
+      case OldTestament.Amo:
+        return '아모스';
+      case OldTestament.Oba:
+        return '오바다';
+      case OldTestament.Jon:
+        return '요나';
+      case OldTestament.Mic:
+        return '미가';
+      case OldTestament.Nah:
+        return '나홈';
+      case OldTestament.Hab:
+        return '하박국';
+      case OldTestament.Zep:
+        return '스바냐';
+      case OldTestament.Hag:
+        return '학개';
+      case OldTestament.Zec:
+        return '스가랴';
+      case OldTestament.Mal:
+        return '말라기';
+      default:
+        return '구약성서';
+    }
+  }
+}
