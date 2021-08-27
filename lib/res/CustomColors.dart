815da387262faca43dc81d1021073f1be1cc710d
boring-km <kms0644804@naver.com>
2021-08-28 00:19:49 +0900

    [NB-01] 구약성서/신약성서 목록 리스트로 보여주기

---
+++ lib/res/CustomColors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color beige = Color(0xffffdba4);
+}
--- lib/screen/bible_screen.dart
+++ lib/screen/bible_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nkrv_bible/data/new_testament.dart';
+import 'package:nkrv_bible/data/old_testament.dart';
+import 'package:nkrv_bible/res/CustomColors.dart';
 
 class BibleScreen extends StatefulWidget {
   @override
@@ -9,15 +12,63 @@ class BibleScreen extends StatefulWidget {
 class _BibleScreenState extends State<BibleScreen> {
   @override
   Widget build(BuildContext context) {
+
+    double h = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Center(
-            child: Text("성경 뷰어", style: TextStyle(fontSize: 24),),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+                color: CustomColors.beige,
+                border: Border.all(color: Colors.black, width: 2.0),
+                borderRadius: BorderRadius.circular(4.0)
+            ),
+            height: 0.9 * h,
+            child: ListView(
+                children: _buildTestamentList()
+            ),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> _buildTestamentList() {
+    final textList = <Widget>[];
+    final labelList = [];
+    labelList.add('구약성서');
+    labelList.addAll(OldTestament.values);
+    labelList.add('신약성서');
+    labelList.addAll(NewTestament.values);
+    for (dynamic item in labelList) {
+      if (item is OldTestament) {
+        final OldTestament ot = item;
+        textList.add(
+            Text(
+              ot.label,
+              style: TextStyle(fontSize: 12),
+            )
+        );
+      } else if (item is NewTestament) {
+        final NewTestament nt = item;
+        textList.add(
+            Text(
+              nt.label,
+              style: TextStyle(fontSize: 12),
+            )
+        );
+      } else {
+        textList.add(
+            Text(
+              item,
+              style: TextStyle(fontSize: 20),
+            )
+        );
+      }
+
+    }
+    return textList;
   }
 
 }
