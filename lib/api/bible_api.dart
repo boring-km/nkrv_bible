d8e42b795ee8aef98cbf28a7412834feb482a7e5
boring-km <kms0644804@naver.com>
2021-08-28 21:03:47 +0900

    [NB-01] 각 성경의 책마다 몇장 몇절까지 있는지 조회하는 api 테스트 추가

--- lib/api/bible_api.dart
+++ lib/api/bible_api.dart
@@ -34,4 +34,12 @@ class BibleAPI {
     final jsonBody = jsonDecode(body);
     return jsonBody['text'] as List<dynamic>;
   }
+
+  static Future<List> getBookCountList(String longLabel) async {
+    final uri = Uri.parse('http://ec2-3-38-12-80.ap-northeast-2.compute.amazonaws.com/count/$longLabel');
+    final response = await http.get(uri);
+    final body = response.body;
+    final jsonBody = jsonDecode(body);
+    return jsonBody['result'] as List<dynamic>;
+  }
 }
--- test/api/bible_api_test.dart
+++ test/api/bible_api_test.dart
@@ -1,19 +1,22 @@
+import 'package:logger/logger.dart';
 import 'package:nkrv_bible/api/bible_api.dart';
 import 'package:test/test.dart';
 
 void main() {
+
+  const longLabel = '창세기';
+  var logger = Logger();
+
   test("창세기 1장 1절 조회하기", () async {
-    final longLabel = '창세기';
-    final paragraph = 1;
-    final chapter = 1;
+    const paragraph = 1;
+    const chapter = 1;
     final result = await BibleAPI.searchOne(longLabel, paragraph, chapter);
     expect('<천지 창조> 태초에 하나님이 천지를 창조하시니라', result);
   });
 
   test("창세기 1장 1-3절 조회하기", () async {
-    final longLabel = '창세기';
-    final paragraph = 1;
-    final chapters = '1-3';
+    const paragraph = 1;
+    const chapters = '1-3';
     final resultArray = await BibleAPI.searchMultiLines(longLabel, paragraph, chapters);
     expect('<천지 창조> 태초에 하나님이 천지를 창조하시니라', resultArray[0]['sentence']);
     expect('땅이 혼돈하고 공허하며 흑암이 깊음 위에 있고 하나님의 영은 수면 위에 운행하시니라', resultArray[1]['sentence']);
@@ -21,15 +24,21 @@ void main() {
   });
 
   test("창세기 1장 전체를 조회하면 31절까지 있다", () async {
-    final longLabel = '창세기';
-    final paragraph = 1;
+    const paragraph = 1;
     final resultArray = await BibleAPI.searchParagraph(longLabel, paragraph);
     expect(31, resultArray.length);
   });
 
   test("창세기 전체를 조회하면 총 1533절이 있다", () async {
-    final longLabel = '창세기';
     final resultArray = await BibleAPI.searchLongLabel(longLabel);
     expect(1533, resultArray.length);
+  });
+
+  test("창세기는 50장까지 있다", () async {
+    final resultList = await BibleAPI.getBookCountList(longLabel);
+    for (var count in resultList) {
+      logger.i('${count['chapter']}장은 ${count['count']}절까지 있다.');
+    }
+    expect(50, resultList.length);
   });
 }
