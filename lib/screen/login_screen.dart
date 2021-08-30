ebaff5cbd9ee6cf89f99c255987ff686cc6feab7
navyCK <ckck81026@gmail.com>
2021-08-30 14:51:31 +0900

    [NB-02] 카카오톡 로그인 성공 화면 표시, 카카오톡 미설치 시 오류 수정

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -8,14 +8,83 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  Future<void> _loginButtonPressed() async {
-    String authCode = await AuthCodeClient.instance.request();
-    print(authCode);
+  bool _isKakaoTalkInstalled = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initKakaoTalkInstalled();
+  }
+
+  _initKakaoTalkInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    print('kakao Install : ' + installed.toString());
+
+    setState(() {
+      _isKakaoTalkInstalled = installed;
+    });
+  }
+
+  _issueAccessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      print(token);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginDone()),
+      );
+    } catch (e) {
+      print("error on issuing access token: $e");
+    }
+  }
+
+  _loginWithKakao() async {
+    try {
+      var code = await AuthCodeClient.instance.request();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _loginWithTalk() async {
+    try {
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  logOutTalk() async {
+    try {
+      var code = await UserApi.instance.logout();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  unlinkTalk() async {
+    try {
+      var code = await UserApi.instance.unlink();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
   }
 
 
   @override
   Widget build(BuildContext context) {
+    isKakaoTalkInstalled();
+
     return Scaffold(
       body: SafeArea(
         minimum: const EdgeInsets.symmetric(horizontal: 16),
@@ -25,10 +94,10 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               width: MediaQuery.of(context).size.width,
               child: CupertinoButton(
-                onPressed: _loginButtonPressed,
+                onPressed: _isKakaoTalkInstalled ? _loginWithTalk : _loginWithKakao,
                 color: Colors.yellow,
                 child: Text(
-                  '카카오 로그인',
+                  '카카오톡 로그인',
                   style: TextStyle(
                     fontSize: 15,
                     color: Colors.black87,
@@ -42,4 +111,32 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+}
+
+class LoginDone extends StatelessWidget {
+  Future<bool> _getUser() async {
+    bool userBool = false;
+    try {
+      User user = await UserApi.instance.me();
+      print(user.toString());
+      userBool = true;
+    } on KakaoAuthException {
+      userBool = false;
+    } catch (e) {
+      userBool = false;
+    }
+    return userBool;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getUser();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Login Success!'),
+        ),
+      ),
+    );
+  }
 }
