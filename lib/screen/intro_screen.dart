b3b4ddcfe9d4580fc0a0fed731b14ffaf00f332d
boring-km <kms0644804@naver.com>
2021-09-20 01:02:47 +0900

    [NB-04] 성경 이미지와 개역개정 성경 제목을 띄운 후 로그인 화면으로 이동

--- lib/screen/intro_screen.dart
+++ lib/screen/intro_screen.dart
@@ -1,31 +1,79 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:nkrv_bible/screen/login_screen.dart';
+
 
 class IntroScreen extends StatelessWidget {
 
-  const IntroScreen({Key? key}) : super(key: key);
+  IntroScreen({Key? key}) : super(key: key) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.offAll(const LoginScreen(), transition: Transition.fade);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    double base = w > h ? h * (3/4) : w * (3/4) ;
+
+    final bottomTextShadow = [
+      const Shadow(
+        color: Colors.white,
+        offset: Offset(1, 1),
+        blurRadius: 10,
+      ),
+      const Shadow(
+        color: Colors.white,
+        offset: Offset(-1, -1),
+        blurRadius: 10,
+      )
+    ];
+
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("로딩화면"),
-                ElevatedButton(onPressed: () {
-                  Get.toNamed('/main');
-                }, child: const Text("메인화면")),
-                ElevatedButton(onPressed: () {
-                  Get.toNamed('/login');
-                }, child: const Text("로그인 화면"))
-              ],
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                width: base,
+              ),
             ),
-          ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: base / 10),
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                    fontSize: base/4,
+                    color: Colors.white,
+                    shadows: bottomTextShadow,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      FadeAnimatedText('개역개정 성경', duration: const Duration(seconds: 3),),
+                    ],
+                    isRepeatingAnimation: false,
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Lottie.asset(
+                'assets/lottie/54020-bible.json',
+                width: base,
+                height: base,
+              ),
+            ),
+          ],
         ),
       ),
     );
