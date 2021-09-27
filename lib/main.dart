57188a5617dfdd63740fcc62284eebf4abc570c1
navyCK <ckck81026@gmail.com>
2021-09-27 11:09:38 +0900

    [NB-05] 카카오 클라이언트 ID 등록

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk/all.dart';
 import 'package:nkrv_bible/screen/bible_screen.dart';
 import 'package:nkrv_bible/screen/book_select_screen.dart';
 import 'package:nkrv_bible/screen/login_screen.dart';
 import 'package:nkrv_bible/screen/main_screen.dart';
 
+import 'app.dart';
+
 void main() {
+  KakaoContext.clientId = "b17b8b41f1eed93570aaf9e64f53ded8";
+  KakaoContext.javascriptClientId = "b7b85e862024c88423792cfb8d0ada4b";
   runApp(const BibleApp());
 }
 
@@ -24,9 +29,9 @@ class BibleApp extends StatelessWidget {
       ),
       initialRoute: '/',
       getPages: [
-        GetPage(name: '/', page: () => IntroScreen(), transition: Transition.fade),
-        GetPage(name: '/login', page: () => const LoginScreen(), transition: Transition.fade),
-        GetPage(name: '/main', page: () => const MainScreen()),
+        GetPage(name: '/', page: () => App()),
+        GetPage(name: '/login', page: () => const LoginScreen()),
+        GetPage(name: '/main', page: () => MainScreen()),
         GetPage(name: '/bible/select', page: () => const BookSelectScreen()),
         GetPage(name: '/bible', page: () => const BibleScreen()),
       ],
