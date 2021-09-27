f9141d4b6e70e7eff8cb702d5850ad2c36baa070
navyCK <ckck81026@gmail.com>
2021-09-27 11:10:13 +0900

    [NB-05] 로그인 이후 화면

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,33 +1,89 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:nkrv_bible/screen/book_select_screen.dart';
+import 'package:logger/logger.dart';
 
-class MainScreen extends StatelessWidget {
+import 'intro_screen.dart';
+import 'login_screen.dart';
 
-  const MainScreen({Key? key}) : super(key: key);
+// ignore: use_key_in_widget_constructors
+class MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final Logger logger = Logger();
+
+  Future <LoginScreen> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+    return const LoginScreen();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("메인화면"),
-                ElevatedButton(onPressed: () {
-                  Get.to(const BookSelectScreen());
-                }, child: const Text("성경 보기"),),
-              ],
-            ),
-          ),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (!snapshot.hasData) {
+            logger.d("로그인을 해주세요.");
+            return const IntroScreen();
+          } else if (snapshot.data!.displayName == null || snapshot.data!.displayName == "") {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("게스트님 환영합니다."),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: CupertinoButton(
+                        color: Colors.blue,
+                        onPressed: _signOut,
+                        child: const Text(
+                          '로그아웃',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("${snapshot.data!.displayName}님 환영합니다."),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: CupertinoButton(
+                        color: Colors.blue,
+                        onPressed: _signOut,
+                        child: const Text(
+                          '로그아웃',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
-
 }
