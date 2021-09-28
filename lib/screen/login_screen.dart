349fc7687bf3f5111763f74f333430d29f19b0b5
navyCK <ckck81026@gmail.com>
2021-09-28 12:02:28 +0900

    [NB-05] 페이스북, 카카오, 네이버, 게스트 로그인 (네이버, 카카오는 기존 앱에서는 정상 작동했지만 본 앱에선 token mismatch error 발생)

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:nkrv_bible/screen/main_screen.dart';
+import 'package:logger/logger.dart';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
 
 // ignore: use_key_in_widget_constructors
 class LoginScreen extends StatefulWidget {
@@ -13,6 +18,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final bool _isDialogVisible = false; // 다이얼로그 visible
+  final Logger logger = Logger();
 
   void _onLoading() {
     showDialog(
@@ -71,6 +77,84 @@ class _LoginScreenState extends State<LoginScreen> {
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+    FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    _onLoading();
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
+  Future<UserCredential> signInWithKaKao() async {
+    final clientState = const Uuid().v4();
+    final url = Uri.https('kauth.kakao.com', '/oauth/authorize', {
+      'response_type': 'code',
+      'client_id': "058ab8ac93b04ab2999fe32ae68199a9",
+      'response_mode': 'form_post',
+      'redirect_uri':
+      'https://pinnate-alpine-passionfruit.glitch.me/callbacks/kakao/sign_in',
+      'state': clientState,
+    });
+
+    final result = await FlutterWebAuth.authenticate(
+        url: url.toString(), callbackUrlScheme: "webauthcallback");
+    final body = Uri.parse(result).queryParameters;
+    logger.d(body["code"]);
+
+    final tokenUrl = Uri.https('kauth.kakao.com', '/oauth/token', {
+      'grant_type': 'authorization_code',
+      'client_id': "058ab8ac93b04ab2999fe32ae68199a9",
+      'redirect_uri':
+      'https://pinnate-alpine-passionfruit.glitch.me/callbacks/kakao/sign_in',
+      'code': body["code"],
+    });
+    var responseTokens = await http.post(Uri.parse(tokenUrl.toString()));
+    Map<String, dynamic> bodys = json.decode(responseTokens.body);
+    var response = await http.post(
+        Uri.parse(
+            'https://pinnate-alpine-passionfruit.glitch.me/callbacks/kakao/token'),
+        body: {"accessToken": bodys['access_token']});
+    _onLoading();
+    return FirebaseAuth.instance.signInWithCustomToken(response.body);
+  }
+
+  Future<UserCredential> signInWithNaver() async {
+    final clientState = const Uuid().v4();
+    final url = Uri.https('nid.naver.com', '/oauth2.0/authorize', {
+      'response_type': 'code',
+      'client_id': "7RuE2n_TrTFDm3gBB8x3",
+      'redirect_uri':
+      'https://pinnate-alpine-passionfruit.glitch.me/callbacks/naver/sign_in',
+      'state': clientState,
+    });
+
+    final result = await FlutterWebAuth.authenticate(
+        url: url.toString(), callbackUrlScheme: "webauthcallback");
+    final body = Uri.parse(result).queryParameters;
+
+    final tokenUrl = Uri.https('nid.naver.com', '/oauth2.0/token', {
+      'grant_type': 'authorization_code',
+      'client_id': "7RuE2n_TrTFDm3gBB8x3",
+      'client_secret': "WRXBa8ZgiA",
+      'code': body["code"],
+      'state': clientState,
+    });
+    var responseTokens = await http.post(Uri.parse(tokenUrl.toString()));
+    Map<String, dynamic> bodys = json.decode(responseTokens.body);
+    var response = await http.post(
+        Uri.parse(
+            "https://pinnate-alpine-passionfruit.glitch.me/callbacks/naver/token"),
+        body: {"accessToken": bodys['access_token']});
+    _onLoading();
+    return FirebaseAuth.instance.signInWithCustomToken(response.body);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,9 +168,26 @@ class _LoginScreenState extends State<LoginScreen> {
                 buildElevatedButton(
                     signInWithGoogle, "assets/icon/icon_google.png", "구글 로그인", const Color(0xffffffff), Colors.black54
                 ),
-                ElevatedButton(onPressed: () {
-                  Get.to(MainScreen());
-                }, child: const Text("메인화면으로 이동")),
+                buildElevatedButton(
+                    signInWithFacebook, "assets/icon/icon_facebook.png", "페이스북 로그인", const Color(0xff3B5998), Colors.white
+                ),
+                // 테스트 모드에선 오류 발생 - 이메일 필수 동의가 아니기 때문에 정식 출시 시 이메일 필수 동의하기.
+                buildElevatedButton(
+                    signInWithKaKao, "assets/icon/icon_kakao.png", "카카오 로그인", const Color(0xffFEE500), Colors.black87
+                ),
+                buildElevatedButton(
+                    signInWithNaver, "assets/icon/icon_naver.png", "네이버 로그인", const Color(0xff1DC800), Colors.white
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text("또는"),
+                ),
+                buildElevatedButton(
+                    _signGuest, "assets/icon/icon_guest.png", "게스트로 이용", const Color(0xffDCDCDC), Colors.black87
+                ),
+                // ElevatedButton(onPressed: () {
+                //   Get.to(MainScreen());
+                // }, child: const Text("메인화면으로 이동")),
                 Visibility(
                     visible: _isDialogVisible,
                     child: Container(
