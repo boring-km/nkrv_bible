3954b666afdd5e2b79e5b8bcfdbd8c889b7a1822
navyCK <ckck81026@gmail.com>
2021-10-12 14:12:21 +0900

    [fix] lottie 적용하여 intro 수정

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:nkrv_bible/app.dart';
 
 // ignore: use_key_in_widget_constructors
@@ -17,39 +19,72 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      const Duration(seconds: 2), () => Get.offAll(App(), transition: Transition.fade),
+      const Duration(seconds: 4), () => Get.offAll(App(), transition: Transition.fade),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    double base = w > h ? h / 2 : w / 2;
 
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/icon/icon_bible.png'), fit: BoxFit.none),
+    final bottomTextShadow = [
+      const Shadow(
+        color: Colors.white,
+        offset: Offset(1, 1),
+        blurRadius: 10,
       ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0.0,
-            left: 0.0,
-            // ignore: sized_box_for_whitespace
+      const Shadow(
+        color: Colors.white,
+        offset: Offset(-1, -1),
+        blurRadius: 10,
+      )
+    ];
+
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(children: [
+          Align(
+            alignment: Alignment.center,
             child: Container(
-              width: width,
-              height: height,
-              child: const Scaffold(
-                backgroundColor: Colors.transparent,
-                body: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Center(),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              width: base,
+              height: base,
+              child: Center(
+                child: Lottie.asset(
+                  'assets/lottie/54020-bible.json',
                 ),
               ),
             ),
           ),
-        ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: base / 10),
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  fontSize: base / 4,
+                  color: Colors.black,
+                  shadows: bottomTextShadow,
+                ),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    FadeAnimatedText(
+                      '개역개정 성경',
+                      duration: const Duration(seconds: 4),
+                    ),
+                  ],
+                  isRepeatingAnimation: true,
+                  repeatForever: true,
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
